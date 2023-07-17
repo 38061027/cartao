18e40ab8c39e014a1e0df2b956ecacea9332e179
José Augusto <joseaugustogomessantos12@gmail.com>
2023-07-16 23:03:08 -0300

    Estilizando e adicionando elementos

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -14,6 +14,11 @@ import {
 export class CardComponent implements AfterViewInit {
   forms: FormGroup;
 
+  valorPreCarregado: string = '1246 8742 9452 7631'
+  namePreCarregado: string = 'José Augusto'
+  vencimentoPreCarregado: string = '02/29'
+  cvcPreCarregado: string = '02/29'
+
   constructor(
     private fb: FormBuilder,
     private element: ElementRef,
@@ -37,6 +42,38 @@ export class CardComponent implements AfterViewInit {
     const numeroFormatado = numero.toString().replace(/\s/g, '').match(/.{1,4}/g);
     return numeroFormatado ? numeroFormatado.join(' ') : '';
   }
+
+
+  
+
+
+  numberPre(event: KeyboardEvent): void {
+    const cardInput = event.target as HTMLInputElement;
+    if (cardInput.value === this.valorPreCarregado) {
+      cardInput.value = '';
+    }
+  }
+
+  namePre(event: KeyboardEvent): void {
+    const cardInput = event.target as HTMLInputElement;
+    if (cardInput.value === this.namePreCarregado) {
+      cardInput.value = '';
+    }
+  }
+
+  valPre(event: KeyboardEvent): void {
+    const cardInput = event.target as HTMLInputElement;
+    if (cardInput.value === this.vencimentoPreCarregado) {
+      cardInput.value = '';
+    }
+  }
+  cvcPre(event: KeyboardEvent): void {
+    const cardInput = event.target as HTMLInputElement;
+    if (cardInput.value === this.cvcPreCarregado) {
+      cardInput.value = '';
+    }
+  }
+
 
   ngAfterViewInit(): void {
 
